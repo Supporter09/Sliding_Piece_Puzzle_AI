3c8e53f1ab3a5132f1f8fecc7a87cc8904177331
Supporter09 <maivannhatminh2005@gmail.com>
2023-09-19 21:21:43 +0700

    commit what DMN dont need

--- src/pages/4x4/index.jsx
+++ src/pages/4x4/index.jsx
@@ -5,143 +5,27 @@ import { Inter } from 'next/font/google'
 import React from "react";
 const inter = Inter({ subsets: ['latin'] })
 
-
-const getShuffledPuzzle = () => {
-  const values = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-
-  const rowOne = [],
-    rowTwo = [],
-    rowThree = [];
-
-  while (values.length) {
-    const random = Math.floor(Math.random() * values.length);
-
-    if (rowOne.length < 3) {
-      rowOne.push(values.splice(random, 1)[0]);
-    } else if (rowTwo.length < 3) {
-      rowTwo.push(values.splice(random, 1)[0]);
-    } else {
-      rowThree.push(values.splice(random, 1)[0]);
-    }
-  }
-
-  return [rowOne, rowTwo, rowThree];
-};
-
-const flattenArray = arr => {
-  return arr.reduce((flatArr, subArr) => flatArr.concat(subArr), []);
-};
-
-// getInversionsCount & isSolvable might not necessary if you use algorithm to generate table
-const getInversionsCount = arr => {
-  arr = flattenArray(arr).filter(n => n !== 0);
-
-  const inversions = [];
-
-  for (let i = 0; i < arr.length - 1; i++) {
-    const currentValue = arr[i];
-    const currentInversions = arr.filter(
-      (val, j) => i < j && val < currentValue
-    );
-    inversions.push(currentInversions.length);
-  }
-
-  const inversionsCount = inversions.reduce((total, val) => total + val, 0);
-
-  return inversionsCount;
-};
-
-const isSolvable = puzzle => {
-  return getInversionsCount(puzzle) % 2 === 0;
-};
-
-const getPuzzle = () => {
-  let puzzle = getShuffledPuzzle();
-
-  while (!isSolvable(puzzle)) {
-    puzzle = getShuffledPuzzle();
-  }
-
-  return puzzle;
-};
-
 export default function Home() {
   const [puzzle, setPuzzle] = React.useState([]);
-  const [complete, setComplete] = React.useState(false);
-  const [moves, setMoves] = React.useState(0);
+  const [complete, setComplete] = React.useState(false); // no need
+  // const [moves, setMoves] = React.useState(0); // no need
 
-  React.useEffect(() => {
-    setPuzzle(getPuzzle());
-  }, []);
-
-  // Check and move piece if possible => The core lead to the change of table is setPuzzle(newPuzzle) => Pass newPuzzle in
-  const movePiece = (x, y) => {
-    if (!complete) {
-      if (checkNeighbours(x, y) || checkNeighbours(x, y, 2)) {
-        const emptySlot = checkNeighbours(x, y) || checkNeighbours(x, y, 2);
-
-        const newPuzzle = puzzle.map(row => row.slice());
-
-        if (x === emptySlot.x && y < emptySlot.y) {
-          newPuzzle[emptySlot.x][emptySlot.y] = puzzle[x][y + 1];
-          newPuzzle[x][y + 1] = newPuzzle[x][y];
-          newPuzzle[x][y] = 0;
-        } else if (x === emptySlot.x && y > emptySlot.y) {
-          newPuzzle[emptySlot.x][emptySlot.y] = puzzle[x][y - 1];
-          newPuzzle[x][y - 1] = newPuzzle[x][y];
-          newPuzzle[x][y] = 0;
-        }
-
-        if (y === emptySlot.y && x < emptySlot.x) {
-          newPuzzle[emptySlot.x][emptySlot.y] = puzzle[x + 1][y];
-          newPuzzle[x + 1][y] = newPuzzle[x][y];
-          newPuzzle[x][y] = 0;
-        } else if (y === emptySlot.y && x > emptySlot.x) {
-          newPuzzle[emptySlot.x][emptySlot.y] = puzzle[x - 1][y];
-          newPuzzle[x - 1][y] = newPuzzle[x][y];
-          newPuzzle[x][y] = 0;
-        }
-
-        setPuzzle(newPuzzle);
-
-        setMoves(moves + 1);
-
-        checkCompletion(newPuzzle);
-      }
-    }
-  };
+  // const A* -> setPuzzle(newMatrix) => Update matrix 
 
   const checkCompletion = puzzle => {
-    if (flattenArray(puzzle).join("") === "123456780") {
-      setComplete(true);
-    }
-  };
+    // if (flattenArray(puzzle).join("") === "123456780") {
+    //   setComplete(true);
+    // } -> no need
 
-  // Check neighbour => Don't need if you use algorithm to generate table
-  const checkNeighbours = (x, y, d = 1) => {
-    const neighbours = [];
 
-    if (puzzle[x][y] !== 0) {
-      neighbours.push(
-        puzzle[x - d] && puzzle[x - d][y] === 0 && { x: x - d, y: y }
-      );
-      neighbours.push(puzzle[x][y + d] === 0 && { x: x, y: y + d });
-      neighbours.push(
-        puzzle[x + d] && puzzle[x + d][y] === 0 && { x: x + d, y: y }
-      );
-      neighbours.push(puzzle[x][y - d] === 0 && { x: x, y: y - d });
-    }
-
-    const emptySlot = neighbours.find(el => typeof el === "object");
-
-    return emptySlot;
+    // setComplete() => only need this
   };
 
   // Reset Puzzle after complete
   const resetPuzzle = () => {
     setComplete(false);
-    setPuzzle(getPuzzle());
-    setMoves(0);
+    // setPuzzle(getPuzzle());
+    // setMoves(0);
   };
 
   return (
@@ -159,7 +43,7 @@ export default function Home() {
         alignItems: 'center',
         height: '100%'
       }} >
-        {<h3>Moves: {moves}</h3>}
+        {/* {<h3>Moves: {moves}</h3>} */}
         <div
           style={{
             display: "inline-block",
@@ -181,7 +65,7 @@ export default function Home() {
                 return (
                   <div
                     key={`${i}-${j}`}
-                    onClick={() => movePiece(i, j)}
+                    // onClick={() => movePiece(i, j)}
                     style={{
                       display: "flex",
                       justifyContent: "center",
--- src/pages/5x5/index.jsx
+++ src/pages/5x5/index.jsx
@@ -6,7 +6,8 @@ import styles from './puzzle.module.css'
 
 export default function Home() {
     const [matrixInitialState, setMatrixInitialState] = useState(null);
-    const [userMatrix, setUserMatrix] = useState()
+    const [userMatrix, setUserMatrix] = useState();
+    const [puzzle, setPuzzle] = React.useState([]);
 
     const handleInputChange = (e) => {
         const inputValue = e.target.value;
@@ -16,20 +17,17 @@ export default function Home() {
     }
 
     const handleClick = () => {
-        setMatrixInitialState(userMatrix)
+        setMatrixInitialState(userMatrix);
+        // setPuzzle(yourPuzzle)
     }
 
+    const playAgain = () => {
+        setMatrixInitialState(null);
+        setUserMatrix()
+    }
     // const handleInputChange = (inputValue) => {
 
     // }
-
-    const handleConfirmClick = () => {
-        // Here, you can update matrixInitialState with the processed input value
-        // For example:
-        // const processedMatrix = ... // Process the input value as needed
-        // setMatrixInitialState(processedMatrix);
-    }
-
 
     return (
         <>
@@ -46,6 +44,50 @@ export default function Home() {
                 <div>
                     <p>Initial Matrix State:</p>
                     {/* Display the initial matrix state here */}
+                    <div
+                        style={{
+                            display: "inline-block",
+                            backgroundColor: "darkgray",
+                            border: `5px solid ${complete ? "black" : "gray"}`,
+                            borderRadius: 5,
+                            padding: 5
+                        }}
+                    >
+                        {puzzle && puzzle.map((row, i) => (
+                            <div
+                                key={i}
+                                style={{
+                                    display: "flex"
+                                }}
+                            >
+                                {row.map((col, j) => {
+                                    const color = col === 0 ? "transparent" : "lightgray";
+                                    return (
+                                        <div
+                                            key={`${i}-${j}`}
+                                            // onClick={() => movePiece(i, j)}
+                                            style={{
+                                                display: "flex",
+                                                justifyContent: "center",
+                                                alignItems: "center",
+                                                width: 77,
+                                                height: 77,
+                                                margin: 2,
+                                                backgroundColor: color,
+                                                borderRadius: 5,
+                                                cursor: complete ? "not-allowed" : "pointer",
+                                                userSelect: "none"
+                                            }}
+                                        >
+                                            <span style={{ fontSize: "2rem", fontWeight: "bold" }}>
+                                                {col !== 0 && col}
+                                            </span>
+                                        </div>
+                                    );
+                                })}
+                            </div>
+                        ))}
+                    </div>
                 </div>
             )}
         </>
